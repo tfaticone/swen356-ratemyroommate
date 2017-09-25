742c1630499e00b72b22ac2d671f09b5ae344c52
Nicholas <nickcfeldman@gmail.com>
2017-09-25 15:28:15 -0400

    Add vue-material

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import VueFire from 'vuefire'
+import VueMaterial from 'vue-material'
 
 import App from './App'
 import router from './router'
@@ -9,6 +10,7 @@ import router from './router'
 Vue.config.productionTip = false
 
 Vue.use(VueFire)
+Vue.use(VueMaterial)
 
 /* eslint-disable no-new */
 new Vue({
