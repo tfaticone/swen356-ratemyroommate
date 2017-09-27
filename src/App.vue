a4a4e307b0dcab80c7d1ad93a3f4a9d11fd3df4a
Nicholas <nickcfeldman@gmail.com>
2017-09-27 17:02:28 -0400

    Move header to partial file
    Move header to partial file

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
   <div id="app">
-    <md-toolbar>
-      <md-button class="md-icon-button">
-        <md-icon>menu</md-icon>
-      </md-button>
-      <h2 class="md-title" style="flex: 1">Rate my Roommate</h2>
-
-      <md-button class="md-icon-button">
-        <md-icon>search</md-icon>
-      </md-button>
-    </md-toolbar>
-
+    <main-nav></main-nav>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import MainNav from './partials/MainNav'
+
+  export default {
+    name: 'app',
+    components: {
+      MainNav
+    }
+  }
 </script>
 
 <style>
-
 </style>
---
+++ src/partials/MainNav.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <md-toolbar>
+      <h2 class="md-title" style="flex: 1">Rate my Roommate</h2>
+
+      <md-button class="md-icon-button">
+        <md-icon>search</md-icon>
+      </md-button>
+    </md-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainNav'
+}
+</script>
+
+<style scoped>
+
+</style>
