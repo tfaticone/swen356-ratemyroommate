6851bc0d1e50cfeedff573be959682b6ae8873ae
Nicholas <nickcfeldman@gmail.com>
2017-09-29 14:54:56 -0400

    Add Login / Sign Up Button

--- src/partials/MainNav.vue
+++ src/partials/MainNav.vue
@@ -6,6 +6,8 @@
       <md-button class="md-icon-button">
         <md-icon>search</md-icon>
       </md-button>
+
+      <md-button>Login / Sign Up</md-button>
     </md-toolbar>
   </div>
 </template>
