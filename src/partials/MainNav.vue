d250f343c1ec5647a3f5d2aa3ef36b145166466a
Nicholas <nickcfeldman@gmail.com>
2017-10-07 09:09:29 -0400

    Show Profile button instead of log in if the auth session exists

--- src/partials/MainNav.vue
+++ src/partials/MainNav.vue
@@ -7,15 +7,29 @@
         <md-icon>search</md-icon>
       </md-button>
 
-      <md-button @click.native="$router.push('/login')">Login / Sign Up</md-button>
+      <md-button v-if="loggedIn">My Profile</md-button>
+      <md-button v-else @click.native="$router.push('/login')">Login / Sign Up</md-button>
+
     </md-toolbar>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'MainNav'
-}
+  import Firebase from 'firebase'
+
+  export default {
+    name: 'MainNav',
+    data() {
+      return {
+        loggedIn: false
+      }
+    },
+    beforeCreate() {
+      Firebase.auth().onAuthStateChanged((user) => {
+        this.loggedIn = user !== null
+      })
+    }
+  }
 </script>
 
 <style scoped>
