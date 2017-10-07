82725e22d7d28edd4653dea3b7a83698cd9abdfa
Nicholas <nickcfeldman@gmail.com>
2017-10-07 11:32:50 -0400

    Add sidenav

--- src/partials/MainNav.vue
+++ src/partials/MainNav.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <md-toolbar>
+      <md-button class="md-icon-button" @click="toggleSideNav">
+        <md-icon>menu</md-icon>
+      </md-button>
+
       <h2 class="md-title" style="flex: 1">Rate my Roommate</h2>
 
       <md-button class="md-icon-button">
@@ -10,7 +14,15 @@
       <md-button v-if="user" @click.native="signOut()">Log Out</md-button>
       <md-button v-else @click.native="$router.push('/login')">Login / Sign Up</md-button>
 
-    </md-toolbar>
+    </md-toolbar >
+
+    <md-sidenav md-swipeable class="md-left" ref="sidenav">
+      <md-toolbar class="md-large">
+        <div class="md-toolbar-container">
+          <h3 class="md-title">Sidenav content</h3>
+        </div>
+      </md-toolbar>
+    </md-sidenav>
   </div>
 </template>
 
@@ -19,7 +31,12 @@
 
   export default {
     name: 'MainNav',
-    mixins: [AuthMixin]
+    mixins: [AuthMixin],
+    methods: {
+      toggleSideNav() {
+        this.$refs['sidenav'].toggle()
+      }
+    }
   }
 </script>
 
