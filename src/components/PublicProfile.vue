d98823b5708539d70911cf61091433096a98489c
Justin Nietzel <jan4426@rit.edu>
2017-10-09 21:32:28 -0400

    Database querying is working. And technically we have a way to view profiles.

---
+++ src/components/PublicProfile.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    {{user}}
+    <h1>{{user.first_name + user.last_name}}</h1>
+  </div>
+</template>
+
+<script>
+  import db from '../database';
+  var schools_ref = db.ref('schools/');
+
+  export default {
+    name : 'profile',
+    data :  function() {
+      var user_ref = schools_ref.child(this.$route.params.school).child(this.$route.params.user);
+      var data = { user : null}
+
+      user_ref.on('value', function (snap) {
+        data.user = snap.val();
+      })
+
+      return data;
+    },
+    firebase : {
+
+
+    }
+  }
+</script>
