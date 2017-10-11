ae7f594fa91ce34a2fbcaeb49a06407213a98976
Justin Nietzel <jan4426@rit.edu>
2017-10-11 13:09:42 -0400

    Steve deleted our production database, now he has to make sure my stuff works after he restores the database

--- src/components/PublicProfile.vue
+++ src/components/PublicProfile.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
-    {{user}}
-    <h1>{{user.first_name + user.last_name}}</h1>
+    <h1>
+      {{user.first_name + " " + user.last_name + " at " + user.college}}
+    </h1>
+    <h2>
+      Average Ratings:
+    </h2>
+    <h3>
+      {{averages}}
+    </h3>
+
   </div>
 </template>
 
@@ -12,18 +20,43 @@
   export default {
     name : 'profile',
     data :  function() {
-      var user_ref = schools_ref.child(this.$route.params.school).child(this.$route.params.user);
-      var data = { user : null}
+      const user_ref = schools_ref.child(this.$route.params.school).child(this.$route.params.user);
+      const data = {
+        user : undefined,
+        averages : undefined,
+      };
 
-      user_ref.on('value', function (snap) {
+      user_ref.on('value', (snap) => {
         data.user = snap.val();
-      })
+
+        const values = {};
+        data.user.reviews.forEach((review) => {
+          Object.keys(review.ratings).forEach((key)=>
+          {
+            if (!values[key]) {
+              values[key] = [review.ratings[key]];
+            }
+            else {
+              values[key].push(review.ratings[key])
+            }
+          });
+        });
+
+        const averages ={};
+        Object.keys(values).forEach((key)=>{
+          let count = 0;
+          let sum = 0;
+          values[key].forEach((num) =>{
+            sum += num;
+            count++;
+          });
+          averages[key] = sum / count;
+        });
+        data.averages = averages;
+
+      });
 
       return data;
-    },
-    firebase : {
-
-
     }
   }
 </script>
