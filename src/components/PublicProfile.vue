d86b15eaca95760f8a3ac7746cd7e58f43e25f3d
smt5511 <smt5511@rit.edu>
2017-10-17 20:11:12 -0400

    prettifies profile interface

--- src/components/PublicProfile.vue
+++ src/components/PublicProfile.vue
@@ -1,29 +1,42 @@
 <template>
   <div>
-    <h1>
+
+    <span class="md-headline">
       {{ user.firstName + " " + user.lastName + " at " + school + " (" + userId + ")" }}
-    </h1>
-    <h2>
-      Overall Rating:
-    </h2>
-    <h2>
-      {{ overallRating }}
-      <md-rating-bar v-model="overallRating" :md-max-rating="5" class="md-primary" :md-empty-icon="'star_border'" disabled></md-rating-bar>
-    </h2>
-    <h3 v-for="(value, key) in ratingAverages">
-      {{ key }}: {{ value }}
-    </h3>
-    <h3 v-for="attr in attributes">
-      {{ attr[0] }}: {{ attr[1] }}
-    </h3>
-    <h2>
-      Reviews:
-    </h2>
-    <div v-for="(review, reviewId) in user.reviews">
-      <div v-for="(value, rating) in review.ratings">
-        {{ rating }}: {{ value }}
-      </div>
-      {{ review.comment }}
+    </span>
+
+    <div>
+      <span class="md-display-2">
+        Overall: {{ overallRating }}
+      </span>
+      <md-rating-bar v-model="overallRating" :md-max-rating=5 class="md-primary" :md-empty-icon="'star_border'" disabled></md-rating-bar>
+    </div>
+
+
+    <md-list class="md-dense">
+      <md-list-item v-for="(value, metric) in userMetrics">
+        <div>
+          <span class="md-title">{{ globalMetrics[metric].desc }}: {{ value }}</span>
+        </div>
+      </md-list-item>
+    </md-list>
+
+    <md-chip disabled v-for="trait in userTraits">
+      {{ globalTraits[trait[0]] }}: +{{ trait[1] }}
+    </md-chip>
+
+    <div>
+      <span class="md-title">
+        {{ Object.keys(user.reviews).length }} Reviews:
+      </span>
+      <md-whiteframe md-tag="section" v-for="(review, reviewId) in user.reviews">
+        <span class="md-title" v-for="(value, metric) in review.ratings">
+          {{ globalMetrics[metric].desc }}: {{ value }}
+        </span>
+        <div>
+          {{ review.comment }}
+        </div>
+      </md-whiteframe md-tag="section">
     </div>
   </div>
 </template>
@@ -33,66 +46,84 @@
   import util from '../util/util';
 
   export default {
-    name : 'profile',
-    data :  function() {
+    name: 'profile',
+    data() {
       const data = {
         school: this.$route.params.school,
         userId: this.$route.params.user,
-        user: undefined,
-        overallRating: undefined,
-        ratingAverages: undefined,
-        attributes: undefined,
+        user: {},
+        overallRating: 0,
+        userTraits: {},
+        userMetrics: {},
+        globalTraits: {},
+        globalMetrics: {},
       };
 
-      const user_ref = db.ref(`schools/${this.$route.params.school}/${this.$route.params.user}`);
-      user_ref.on('value', (snap) => {
-        data.user = snap.val();
+      const metricsRef = db.ref('metrics');
+      metricsRef.on('value', (metricsSnap) => {
+        const globalMetrics = metricsSnap.val();
+        data.globalMetrics = globalMetrics;
 
-        // tally up attributes and ratings
-        const attributes = {}; // smokes, has pet, etc.
-        const ratings = {}; // loudness, politeness, etc.
-        Object.keys(data.user.reviews).forEach((reviewId) => {
-          const review = data.user.reviews[reviewId];
+        const traitsRef = db.ref('traits');
+        traitsRef.on('value', (traitsSnap) => {
+          const globalTraits = traitsSnap.val();
+          data.globalTraits = globalTraits;
 
-          // attributes
-          Object.keys(review.attributes).forEach((attrKey) => {
-            if (!attributes[attrKey]) {
-              attributes[attrKey] = 0
-            }
-            if (review.attributes[attrKey] === true) {
-              attributes[attrKey] += 1;
-            } else {
-              attributes[attrKey] -= 1;
-            }
-          });
+          const userRef = db.ref(`schools/${this.$route.params.school}/${this.$route.params.user}`);
+          userRef.on('value', (userSnap) => {
+            data.user = userSnap.val();
 
-          // ratings
-          Object.keys(review.ratings).forEach((key) => {
-            if (!ratings[key]) {
-              ratings[key] = [];
-            }
-            ratings[key].push(review.ratings[key]);
+            // tally up metrics and traits
+            const userTraits = {}; // smokes, has pet, etc.
+            const userMetrics = {}; // loudness, politeness, etc.
+            Object.keys(data.user.reviews).forEach((reviewId) => {
+              const review = data.user.reviews[reviewId];
+
+              // userTraits
+              Object.keys(review.traits).forEach((trait) => {
+                if (!userTraits[trait]) {
+                  userTraits[trait] = 0
+                }
+                if (review.traits[trait] === true) {
+                  userTraits[trait] += 1;
+                } else {
+                  userTraits[trait] -= 1;
+                }
+              });
+
+              // userMetrics
+              Object.keys(review.ratings).forEach((key) => {
+                if (!userMetrics[key]) {
+                  userMetrics[key] = [];
+                }
+                userMetrics[key].push(review.ratings[key]);
+              });
+            });
+            data.userTraits = util.sortObject(userTraits)
+              .filter(trait => trait[1] > 0);
+
+
+
+            // calculate averages for ease of judging a profile
+            const metricAverages = {};
+            let overallRating = 0;
+            Object.keys(userMetrics).forEach((key)=>{
+              let sum = 0;
+              userMetrics[key].forEach((num) =>{
+                sum += num;
+              });
+              const ratingAverage = sum / userMetrics[key].length;
+              metricAverages[[key]] = (ratingAverage).toFixed(1);
+              overallRating += ratingAverage;
+            });
+
+            data.userMetrics = metricAverages;
+            data.overallRating = (overallRating / Object.keys(userMetrics).length).toFixed(1);
           });
         });
-        data.attributes = util.sortObject(attributes).filter(attribute => attribute[1] > 0);
-
-        // calculate averages for ease of judging a profile
-        const ratingAverages = {};
-        let overallRating = 0;
-        Object.keys(ratings).forEach((key)=>{
-          let sum = 0;
-          ratings[key].forEach((num) =>{
-            sum += num;
-          });
-          const ratingAverage = sum / ratings[key].length;
-          ratingAverages[key] = (ratingAverage).toFixed(1);
-          overallRating += ratingAverage;
-        });
-        data.ratingAverages = ratingAverages;
-        data.overallRating = (overallRating / Object.keys(ratings).length).toFixed(1);
       });
 
       return data;
-    }
+    },
   }
 </script>
